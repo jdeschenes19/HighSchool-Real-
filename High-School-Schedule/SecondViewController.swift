6a2ea461af77eca896619572bdabb206a307b7b5
jdeschenes19 <jdeschenes19@students.polytechnic.org>
2015-06-01 14:45:42 -0700

    stuff

--- High-School-Schedule/SecondViewController.swift
+++ High-School-Schedule/SecondViewController.swift
@@ -10,47 +10,119 @@ import UIKit
 
 var classDict = ["aPeriod": "", "bPeriod": "", "cPeriod": "", "dPeriod": "", "ePeriod": "", "fPeriod": "", "xPeriod": "", "sport": ""]
 
-var classArray = [String]()
-
-func updateArray() {
-    
-    classArray.removeAll(keepCapacity: false)
-    
-    classArray.append(classDict["aPeriod"]!)
-    
-    classArray.append(classDict["bPeriod"]!)
-    
-    classArray.append(classDict["cPeriod"]!)
-    
-    classArray.append(classDict["dPeriod"]!)
-    
-    classArray.append(classDict["ePeriod"]!)
-    
-    classArray.append(classDict["fPeriod"]!)
-    
-    classArray.append(classDict["xPeriod"]!)
-    
-    classArray.append(classDict["sport"]!)
-    
-    println(classDict["aPeriod"])
-    
-    println(classDict["bPeriod"])
-    
-    println(classDict["cPeriod"])
-    
-    println(classDict["dPeriod"])
-    
-    println(classDict["ePeriod"])
-    
-    println(classDict["fPeriod"])
-    
-    println(classDict["xPeriod"])
-    
-    println(classDict["sport"])
-    
-}
+var classArray = ["A Period", "", "", "", "", "", "", "", ""]
 
 class SecondViewController: UIViewController {
+    
+    var weekday = 0
+    
+    func updateArray() {
+        
+        if weekday == 2 || weekday == 7 || weekday == 1 || weekday == 0 {
+            
+            classArray.removeAll(keepCapacity: false)
+            
+            classArray.append(classDict["xPeriod"]!)
+            
+            classArray.append(classDict["aPeriod"]!)
+            
+            classArray.append(classDict["bPeriod"]!)
+            
+            classArray.append("Morning Meeting")
+            
+            classArray.append(classDict["cPeriod"]!)
+            
+            classArray.append(classDict["dPeriod"]!)
+            
+            classArray.append("Lunch")
+            
+            classArray.append(classDict["ePeriod"]!)
+            
+            classArray.append(classDict["fPeriod"]!)
+            
+            classArray.append(classDict["sport"]!)
+            
+            println(classArray)
+            
+        } else if weekday == 3 {
+            
+            classArray.removeAll(keepCapacity: false)
+            
+            classArray.append("M Period")
+            
+            classArray.append(classDict["aPeriod"]!)
+            
+            classArray.append("Advisee")
+            
+            classArray.append(classDict["cPeriod"]!)
+            
+            classArray.append(classDict["xPeriod"]!)
+            
+            classArray.append("Lunch")
+            
+            classArray.append(classDict["fPeriod"]!)
+            
+        } else if weekday == 4 {
+            
+            classArray.removeAll(keepCapacity: false)
+            
+            classArray.append(classDict["aPeriod"]!)
+            
+            classArray.append(classDict["dPeriod"]!)
+            
+            classArray.append(classDict["xPeriod"]!)
+            
+            classArray.append("Lunch")
+            
+            classArray.append(classDict["fPeriod"]!)
+            
+        } else if weekday == 5 {
+            
+            classArray.removeAll(keepCapacity: false)
+            
+            classArray.append(classDict["dPeriod"]!)
+            
+            classArray.append(classDict["aPeriod"]!)
+            
+            classArray.append("Advisee")
+            
+            classArray.append(classDict["fPeriod"]!)
+            
+            classArray.append(classDict["cPeriod"]!)
+            
+            classArray.append(classDict["xPeriod"]!)
+            
+            classArray.append("Lunch")
+            
+            classArray.append(classDict["bPeriod"]!)
+            
+            classArray.append(classDict["ePeriod"]!)
+            
+        } else if weekday == 6 {
+            
+            classArray.append(classDict["bPeriod"]!)
+            
+            classArray.append(classDict["fPeriod"]!)
+            
+            classArray.append("Assembly")
+            
+            classArray.append(classDict["ePeriod"]!)
+            
+            classArray.append(classDict["xPeriod"]!)
+            
+            classArray.append("Lunch")
+            
+            classArray.append(classDict["cPeriod"]!)
+            
+            classArray.append(classDict["dPeriod"]!)
+            
+        }
+        
+        defaults.setObject(classArray, forKey: "storedClassArray")
+        
+        defaults.setObject(classDict, forKey: "storedClassDict")
+        
+    }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         
@@ -66,6 +138,8 @@ class SecondViewController: UIViewController {
         
         updateArray()
         
+        println(classDict["aPeriod"])
+        
     }
     
     @IBOutlet var field: UITextField!
@@ -73,6 +147,8 @@ class SecondViewController: UIViewController {
     @IBAction func aPeriodButton(sender: UIButton) {
         
         updateClasses("aPeriod")
+        
+        println(classDict["aPeriod"])
         
     }
     
@@ -122,6 +198,36 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         
         updateArray()
+        
+        println("View did load")
+        
+        var DateInFormat:String = ""
+        
+        var todaysDate:NSDate = NSDate()
+        var dateFormatter:NSDateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        DateInFormat = dateFormatter.stringFromDate(todaysDate)
+        
+        func getDayOfWeek(today:String)->Int? {
+            
+            let formatter  = NSDateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+            if let todayDate = formatter.dateFromString(today) {
+                let myCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+                let myComponents = myCalendar.components(.CalendarUnitWeekday, fromDate: todayDate)
+                let weekDay = myComponents.weekday
+                return weekDay
+            } else {
+                return nil
+            }
+        }
+        
+        
+        if let weekday = getDayOfWeek(DateInFormat) {
+            println("Weekday is \(weekday) on second view controller.")
+        } else {
+            println("bad input")
+        }
         
     }
 
